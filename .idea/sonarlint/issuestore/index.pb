e8b09298a45515cd8b551848393bfdb398032dfe
CaptStormCrow <kpbond@hotmail.com>
2021-04-27 09:22:33 -0500

    not errors in main

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,8 +5,6 @@ i
 9src/main/java/com/bbank/dbutil/PostgresSqlConnection.java,9\d\9de10edf0348d62965b70eaaef6947ea78b28f7f
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-Y
-)src/main/java/com/bbank/dao/BBankDAO.java,7\0\707b56d5b934c975bce9e238b78d3e0e27f05481
 \
 ,src/main/java/com/bbank/dao/EmployeeDAO.java,5\d\5d788d94bf129ccf85256f1002949926d4fc4179
 [
@@ -15,3 +13,5 @@ _
 /src/main/java/com/bbank/dao/impl/BBankImpl.java,7\9\79d62872a65f86f3cd297ec984a88a96b0772fa5
 U
 %src/main/java/com/bbank/MainBank.java,4\c\4cc806e4b78e9853329e781f0860fa98223bc187
+[
++src/main/java/com/bbank/model/Employee.java,7\4\7418340c567a892e3d87bbe1f18655952e5b89b8
--- src/main/java/com/bbank/MainBank.java
+++ src/main/java/com/bbank/MainBank.java
@@ -20,75 +20,81 @@ public class MainBank implements EmployeeDAO {
     public static void main(String[] args) {
 
     }
-        public void mainMenu(Scanner scan) {
-            int mMenu = 0;
-            do {
-                try {
-                    log.info("\n\nMain Menu. Please select an option: ");
-                    log.info("1. Accounts");
-                    log.info("2. Apply for new account");
-                    log.info("3. Employee Menu");
-                    log.info("4. Exit");
-                    int selection = Integer.parseInt(scan.nextLine());
-                    mMenu = selection;
-                } catch (Exception e){
-                    log.info("Invalid Entry!");
-                    mMenu =0;
+
+    public void mainMenu(Scanner scan) {
+        int mMenu;
+        do {
+            try {
+                System.out.println("\n\nMain Menu. Please select an option: ");
+                System.out.println("1. Accounts");
+                System.out.println("2. Apply for new account");
+                System.out.println("3. Employee Menu");
+                System.out.println("4. Exit");
+                mMenu = Integer.parseInt(scan.nextLine());
+            } catch (Exception e) {
+                System.out.println("Invalid Entry!");
+                mMenu = 0;
+            }
+
+            switch (mMenu) {
+                case 1:
+                    signInCustomer(scan);
+                    break;
+                case 2:
+                    this.signUpNewCustomer(scan);
+                    break;
+                case 3:
+                    this.employeeMenu(scan);
+                    break;
+                default:
+                    System.out.println("Please enter a number between 1 and 3");
+            }
+        } while (mMenu != 4);
+    }
+
+
+
+    private void signInCustomer (Scanner scan){
+        boolean isSignedIn = false;
+        String password = null;
+        String username = null;
+        while (!isSignedIn) {
+            BBankImpl bankDao = new BBankImpl();
+            Customer customer = new Customer();
+            System.out.println("\n\nWelcome back. Please enter username: ");
+            System.out.println("Or type exit to return to main menu.");
+            username = scan.nextLine();
+            if (username.matches("Exit")){
+                System.out.println("Returning to Main Menu");
+            }else {
+                System.out.println("\nThank you, "+username+ " . Enter your password");
+                password = scan.nextLine();
+
+                try{
+                    customer = this.loginVerification(username, password);
+                    Exception ex1 = new BusinessException() ;
+
+                    SQLException ex2 = new SQLException(ex1);
+                    throw ex2;
+                } catch(SQLException e){
+                    System.out.println(e.getMessage());
                 }
 
-                switch (mMenu){
-                    case 1: signInCustomer(scan);
-                        break;
-                    case 2: this.signUpNewCustomer(scan);
-                        break;
-                    case 3: this.employeeMenu(scan);
-                        break;
-                    default: log.info("Please enter a number between 1 and 3");
-                }
-            }while(mMenu != 4);
-        }
 
-        private void signInCustomer (Scanner scan){
-            boolean isSignedIn = false;
-            String password = null;
-            String username = null;
-            while (!isSignedIn) {
-                BBankImpl bankDao = new BBankImpl();
-                Customer customer = new Customer();
-                log.info("\n\nWelcome back. Please enter username: ");
-                log.info("Or type exit to return to main menu.");
-                username = scan.nextLine();
-                if (username.matches("Exit")){
-                    log.info("Returning to Main Menu");
-                }else {
-                    log.info("\nThank you, "+username+ " . Enter your password");
-                    password = scan.nextLine();
-
-                    try{
-                        customer = this.loginVerification(username, password);
-                        Exception ex1 = new BusinessException() ;
-
-                        SQLException ex2 = new SQLException(ex1);
-                        throw ex2;
-                    } catch(SQLException e){
-                        log.info(e.getMessage());
-                    }
-
-
-                    log.info("Signing in.");
-                    isSignedIn = true;
-                    this.customerMenu(scan, customer);
-                }
+                System.out.println("Signing in.");
+                isSignedIn = true;
+                this.customerMenu(scan);
             }
         }
+    }
 
-        public void customerMenu(Scanner scan, Customer customer){
+        public void customerMenu(Scanner scan){
             int startMenu = 0;
             while (startMenu !=3){
-                log.info("\n\nCustomer Menu. Please select an option: ");
-                log.info("1. Accounts");
-                log.info("2. Apply for new account");
-                log.info("3. Exit");
+                System.out.println("\n\nCustomer Menu. Please select an option: ");
+                System.out.println("1. Accounts");
+                System.out.println("2. Apply for new account");
+                System.out.println("3. Exit");
 
                 startMenu = Integer.parseInt(scan.nextLine());
 
@@ -97,49 +103,69 @@ public class MainBank implements EmployeeDAO {
                         break;
                     case 2 :
                         break;
-                    case 3 : log.info("Returning to Main Menu");
+                    case 3 : System.out.println("Returning to Main Menu");
                         break;
-                    default: log.info("Please enter a number between 1 and 3");
+                    default: System.out.println("Please enter a number between 1 and 3");
                 }
             }
         }
 
-        private void signUpNewCustomer(Scanner scan, Customer customer){
+        private void signUpNewCustomer(Scanner scan){
            boolean isSignedUp = false;
            String username=null;
            String password=null;
            String firstName=null;
            String lastName=null;
             while (isSignedUp){
-                log.info("\n\nNew Customer Sign up. Please create a username: ");
+                System.out.println("\n\nNew Customer Sign up. Please create a username: ");
                 username = scan.nextLine();
                 if (username.matches("Exit")){
-                    log.info("Returning to Main Menu!");
+                    System.out.println("Returning to Main Menu!");
                 }else{
-                    log.info("Create a password: ");
+                    System.out.println("Create a password: ");
                     password = scan.nextLine();
-                    log.info("Please enter your First Name: ");
+                    System.out.println("Please enter your First Name: ");
                     firstName = scan.nextLine();
-                    log.info("Please enter your Last Name: ");
+                    System.out.println("Please enter your Last Name: ");
                     lastName = scan.nextLine();
 
-                    log.info("First Name: "+firstName+", \nLast Name: "+lastName+", \nUsername: " +username+" ,\nPassword: "+password+". \nIs this correct? Y/N");
+                    System.out.println("First Name: "+firstName+", \nLast Name: "+lastName+", \nUsername: " +username+" ,\nPassword: "+password+". \nIs this correct? Y/N");
                     String answer = scan.nextLine();
                     if(answer.matches("[yY]")){
-                        log.info("User Created!");
-                        log.info("\nRedirecting to Customer Menu. New Customers must still apply for a new account.");
-                        customer = new Customer();
+                        System.out.println("User Created!");
+                        System.out.println("\nRedirecting to Customer Menu. New Customers must still apply for a new account.");
+                        Customer customer = new Customer();
                     }
 
-                    log.info("Signing in.");
+                    System.out.println("Signing in.");
                     isSignedUp = true;
-                    this.customerMenu(scan, customer);
                 }
             }
         }
 
         private void customerAccountMenu(Scanner scan){
+            int custMenu = 0;
+            while (custMenu !=3){
+                System.out.println("\n\nEmployee Menu. Please select an option: ");
+                System.out.println("1. Accounts");
+                System.out.println("2. Open new accounts");
+                System.out.println("3. Deposit or withdraw");
+                System.out.println("4. Exit");
 
+                custMenu = Integer.parseInt(scan.nextLine());
+
+                switch (custMenu){
+                    case 1 : this.customerAccountMenu(scan);
+                        break;
+                    case 2 :
+                        break;
+                    case 3 :
+                        break;
+                    case 4 : System.out.println("Returning to Main Menu");
+                        break;
+                    default: System.out.println("Please enter a number between 1 and 3");
+                }
+            }
         }
 
 
@@ -153,13 +179,13 @@ public class MainBank implements EmployeeDAO {
             while (!isSignedIn) {
                 BBankImpl bankDao = new BBankImpl();
                 Employee employee = new Employee();
-                log.info("\n\nWelcome back. Please enter username: ");
-                log.info("Or type exit to return to main menu.");
+                System.out.println("\n\nWelcome back. Please enter username: ");
+                System.out.println("Or type exit to return to main menu.");
                 username = scan.nextLine();
                 if (username.matches("Exit")){
-                    log.info("Returning to Main Menu");
+                    System.out.println("Returning to Main Menu");
                 }else {
-                    log.info("\nThank you, "+username+" . Enter your password");
+                    System.out.println("\nThank you, "+username+" . Enter your password");
                     password = scan.nextLine();
 
                     try{
@@ -169,10 +195,10 @@ public class MainBank implements EmployeeDAO {
                         SQLException ex2 = new SQLException(ex1);
                         throw ex2;
                     } catch(SQLException e){
-                        log.info(e.getMessage());
+                        System.out.println(e.getMessage());
                     }
 
-                    log.info("Signing in.");
+                    System.out.println("Signing in.");
                     isSignedIn = true;
                     this.employeeMenu(scan);
                 }
@@ -184,11 +210,11 @@ public class MainBank implements EmployeeDAO {
         public void employeeMenu(Scanner scan){
             int empMenu = 0;
             while (empMenu !=3){
-                log.info("\n\nEmployee Menu. Please select an option: ");
-                log.info("1. Accounts");
-                log.info("2. Approve or deny new accounts");
-                log.info("3. Deposit or withdraw");
-                log.info("4. Exit");
+                System.out.println("\n\nEmployee Menu. Please select an option: ");
+                System.out.println("1. Accounts");
+                System.out.println("2. Approve or deny new accounts");
+                System.out.println("3. Deposit or withdraw");
+                System.out.println("4. Exit");
 
                 empMenu = Integer.parseInt(scan.nextLine());
 
@@ -197,15 +223,15 @@ public class MainBank implements EmployeeDAO {
                         break;
                     case 2 :
                         break;
-                    case 3 : log.info("Returning to Main Menu");
+                    case 3 : System.out.println("Returning to Main Menu");
                         break;
-                    default: log.info("Please enter a number between 1 and 3");
+                    default: System.out.println("Please enter a number between 1 and 3");
                 }
             }
         }
-        
+
         public void employeeAccountMenu(Scanner scan){
-        
+
         }
 
 
